124a19dcf1ef2459eb0fdc1859c006b69b5fdd18
Exinker <exinker@gmail.com>
2020-09-21 15:24:40 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 DEBUG = True
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads/')
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'static', 'uploads')
 ALLOWED_EXTENSIONS = {'mp3'}
